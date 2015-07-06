4b9df27235d0431e82d84df9136dc216f945cb7a
Brexton Pham <brextonpham@stanford.edu>
2015-07-06 00:50:04 -0400

    Uploading Weather App

---
+++ Weather/ForecastService.swift
@@ -0,0 +1,42 @@
+//
+//  ForecastService.swift
+//  Weather
+//
+//  Created by Brexton Pham on 7/5/15.
+//  Copyright (c) 2015 Brexton Pham. All rights reserved.
+//
+
+import Foundation
+
+struct ForecastService {
+    let forecastAPIKey: String
+    let forecastBaseURL: NSURL?
+    
+    init(APIKey: String) {
+        forecastAPIKey = APIKey
+        forecastBaseURL = NSURL(string: "https://api.forecast.io/forecast/\(forecastAPIKey)/")
+    }
+    
+    func getForecast(lat: Double, long: Double, completion: (CurrentWeather? -> Void)) {
+        if let forecastURL = NSURL(string: "\(lat),\(long)", relativeToURL: forecastBaseURL) {
+            let networkOperation = NetworkOperation(url: forecastURL)
+            
+            networkOperation.downloadJSONFromURL {
+                (let JSONDictionary) in
+                let currentWeather = self.currentWeatherFromJSON(JSONDictionary)
+                completion(currentWeather)
+            }
+        } else {
+            println("Could not construct a valid URL")
+        }
+    }
+    
+    func currentWeatherFromJSON(jsonDictionary: [String: AnyObject]?) -> CurrentWeather? {
+        if let currentWeatherDictionary = jsonDictionary?["currently"] as? [String: AnyObject] {
+            return CurrentWeather(weatherDictionary: currentWeatherDictionary)
+        } else {
+            println("JSON dictionary returned nil for 'currently' key")
+            return nil
+        }
+    }
+}
